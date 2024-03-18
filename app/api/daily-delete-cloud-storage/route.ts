d4e9ffd2bc3d4ca30064692800e0f736638fc0f3
Caleb <89772184+belacmu@users.noreply.github.com>
2024-03-18 15:50:39 +0100

    cron job test

---
+++ app/api/daily-delete-cloud-storage/route.ts
@@ -0,0 +1,14 @@
+// TEMP EXAMPLE CODE
+import { v2 as cloudinary } from "cloudinary";
+ 
+cloudinary.config({
+  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const callback = (response: any) => {
+    console.log(response);
+  };
+
+cloudinary.api.ping().then(callback);
