95aff2c27f512a5648f93bdf1c5edd4fc57b1c61
Caleb <89772184+belacmu@users.noreply.github.com>
2024-03-20 11:07:07 +0100

    Update route.ts

--- app/api/daily-delete-cloud-storage/route.ts
+++ app/api/daily-delete-cloud-storage/route.ts
@@ -17,7 +17,7 @@ export async function GET() {
   try {
     const result = await cloudinary.api.delete_resources_by_tag('colorchangesigned')
     .then(callback);
-    return Response.json(result);
+    return result;
   } catch (error) {
       console.error('Error deleting tagged files:', error);
       throw new Error('An error occurred while deleting tagged files');
