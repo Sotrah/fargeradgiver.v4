ad249097bf8a4c3ec464ee033a17a85bedeb60a7
Caleb <89772184+belacmu@users.noreply.github.com>
2024-03-24 11:29:55 +0100

    moved upload button out of grid, new uploaded images always in position 4

--- components/ImageGridCard.tsx
+++ components/ImageGridCard.tsx
@@ -11,11 +11,11 @@ interface ImageGridCardProps {
 const ImageGridCard: React.FC<ImageGridCardProps> = ({ onPictureSelect }) => {
     const [selected, setSelected] = useState<number>(0);
 
-    const images: string[] = [
+    const [images, setImages] = useState<string[]>([
         'https://res.cloudinary.com/dj6mfsxnu/image/upload/v1711181504/e5rhfxd4qbo6a2irtfqn.jpg',
         'https://res.cloudinary.com/dj6mfsxnu/image/upload/v1711181537/oqa7u4fxuyknbxyfxn9a.jpg',
         'https://res.cloudinary.com/dj6mfsxnu/image/upload/v1711181638/haxttwqwqq11kz8hmys8.jpg',
-    ];
+    ]);
 
     const handleImageClick = (index: number): void => {
         setSelected(index);
@@ -25,36 +25,41 @@ const ImageGridCard: React.FC<ImageGridCardProps> = ({ onPictureSelect }) => {
 
     // Function to handle the state update in the parent component
     const handleUploadSuccess = (result: string): void => {
-        setSelected(images.length); // Assuming this is meant to set selected to a new, unique value indicating an upload
+        const updatedImages = images
+        updatedImages[3] = result; // Add the uploaded photo to the images array at index 3
+        setImages(updatedImages);
+        setSelected(3); // uploaded photo always in same spot
         console.log(result);
         onPictureSelect(result); // "Feed" the selected picture url to the parent component
     };
 
     return (
-        <div className="grid lg:grid-cols-2 grid-cols-4 gap-2 justify-start">
-            {images.map((src, index) => (
-                <div
-                    key={index}
-                    className={`w-full rounded-lg flex items-center justify-center overflow-hidden relative border-2 ${selected === index ? 'border-black' : 'border-transparent'}  hover:border-gray-500`}
-                    onClick={() => handleImageClick(index)}
-                >
-                    <CloudinaryWrapper
-                        width={500}
-                        height={500}
-                        src={src}
-                        alt={`Image ${index + 1}`}
-                        layout="responsive"
-                        objectFit="cover"
-                    />
-                </div>
-            ))}
-            <div
-                key={images.length}
-                className={`relative border-2 ${selected === images.length ? 'border-black' : 'border-transparent'} rounded-lg`}
-                onClick={() => handleUploadSuccess('')} // You might need a different handler here
-            >
-                <UploadButton onUploadSuccess={handleUploadSuccess} />
+        <div>
+            <div className="grid lg:grid-cols-2 grid-cols-4 gap-2 justify-start">
+                {images.map((src, index) => (
+                    <div
+                        key={index}
+                        className={`w-full rounded-lg flex items-center justify-center overflow-hidden relative border-2 ${selected === index ? 'border-black' : 'border-transparent'}  hover:border-gray-500`}
+                        onClick={() => handleImageClick(index)}
+                    >
+                        <CloudinaryWrapper
+                            width={500}
+                            height={500}
+                            src={src}
+                            alt={`Image ${index + 1}`}
+                            layout="responsive"
+                            objectFit="cover"
+                        />
+                    </div>
+                ))}
+                
             </div>
+            <div
+                    className={`relative border-2  rounded-lg`}
+                    // onClick={() => handleUploadSuccess('')} // You might need a different handler here
+                >
+                    <UploadButton onUploadSuccess={handleUploadSuccess} />
+                </div>
         </div>
     );
 }
