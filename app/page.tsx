2bab45d42c41e3e7924d231ee80d3214936169f5
Caleb <89772184+belacmu@users.noreply.github.com>
2024-03-27 11:25:13 +0100

    small fix

--- app/page.tsx
+++ app/page.tsx
@@ -108,7 +108,7 @@ export default function Home() {
                   <div className="w-12 h-12 bg-gray-100 rounded-full"></div>
                   <div className="w-12 h-12 bg-gray-100 rounded-full"></div>
                   <div className="w-12 h-12 bg-gray-100 rounded-full"></div>
-                  <div className="w-12 h-12 bg-gray-100 rounded-full"></div>
+
                   </div>
               </div>
               
