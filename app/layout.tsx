57892e664f5a1d8129d55a7460f84d230a8ddfa6
Caleb <89772184+belacmu@users.noreply.github.com>
2024-04-03 11:30:49 +0200

    added microsoft clarity tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={inter.className}>{children}</body>
+            <script type="text/javascript">
+                {`(function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "lqr4yhbjsy");`}
+            </script>
+            <body className={inter.className}>{children}</body>
         </html>
     );
 }
